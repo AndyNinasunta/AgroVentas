033a285c932579a585903594ec2958c52d03450d
ijeysonU <jeyson.jaramillo2016@uteq.edu.ec>
2022-03-03 20:26:54 -0500

    Subida ws

---
+++ back-end/wsAgroventas/src/java/wsAgroVentas/wUsuarios.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wsAgroVentas;
+
+import java.sql.SQLException;
+
+/**
+ *
+ * @author jeyso
+ */
+public class wUsuarios {
+
+    conexion cc = new conexion();
+
+    public String prcLogin(String us, String ps) {
+        String qry = "SELECT * FROM fnLogin ('" + us + "', '" + ps + "')";
+        String dataResp = cc.getRecordsInJson(qry);
+        return dataResp;
+    }
+
+    public String prcRegCliente(String nm, String dr, String ruc,
+            String ml, String tl) throws SQLException {
+        String fin = "";
+        String vrf = "select * from fnVerificaCliente ('" + ruc + "')";
+        String qry = "select * from fnRegistrarCliente ('" + nm + "', '" + dr
+                + "', '" + ruc + "', '" + ml + "', '" + tl + "')";
+        String val = cc.Registro(vrf);
+        if ("Registrar".equals(val)) {
+            cc.EjecutarSQL(qry);
+            fin = cc.Registro(vrf);
+        } else {
+            fin = val;
+        }
+        return fin;
+    }
+
+    public String prcDataCliente(String ruc) {
+        String qry = "select * from fnObtenerDatosCliente ('" + ruc + "')";
+        String dataCliente = cc.getRecordsInJson(qry);
+        return dataCliente;
+    }
+    
+    
+    
+    
+}
